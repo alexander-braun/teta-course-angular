0dc050a938b1d221a1227238f41ac392f6ce219c
aleksanderbr <alexanderbraun@freenet.de>
2020-11-04 22:04:23 +0100

    finished typing

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence(15);
+  textCorrect = false;
+  enteredText = '';
+
+  onInput(text: string): void {
+    if (this.randomText === text) {
+      this.textCorrect = true;
+    }
+    this.enteredText = text;
+  }
+
+  compare(char: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'black';
+    } else if (char === enteredLetter) {
+      return 'green';
+    } else {
+      return 'red';
+    }
+  }
 }
