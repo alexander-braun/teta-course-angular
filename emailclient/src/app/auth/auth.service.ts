87865866d37e41e015a259665bc1163bfdc8bd73
Alex <alexanderbraun@freenet.de>
2020-11-21 20:48:44 +0100

    started signup

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -2,18 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
+interface UsernameResponse {
+  available: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com/auth/';
   constructor(private httpClient: HttpClient) {}
 
-  usernameAvailable(username: string): Observable<{ available: boolean }> {
-    return this.httpClient.post<{ available: boolean }>(
-      'https://api.angular-email.com/auth/username',
-      {
-        username,
-      }
+  usernameAvailable(username: string): Observable<UsernameResponse> {
+    return this.httpClient.post<UsernameResponse>(this.rootUrl + 'username', {
+      username,
+    });
+  }
+
+  signup(credentials: SignupCredentials): Observable<SignupResponse> {
+    return this.httpClient.post<SignupResponse>(
+      this.rootUrl + 'signup',
+      credentials
     );
   }
 }
--- emailclient/src/app/auth/signup/signup.component.ts
+++ emailclient/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
 
@@ -39,8 +41,24 @@ export class SignupComponent implements OnInit {
 
   constructor(
     private matchPassword: MatchPassword,
-    private uniqueUsername: UniqueUsername
+    private uniqueUsername: UniqueUsername,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.authForm.invalid) {
+      return;
+    }
+
+    this.authService.signup(this.authForm.value).subscribe({
+      next: (response) => {},
+      error: (err) => {
+        if (!err.status) {
+          this.authForm.setErrors({ noConnection: true });
+        }
+      },
+    });
+  }
 }
