c774671c9f40b9c46392fe29718bfbc7aaf25d68
Alex <alexanderbraun@freenet.de>
2020-11-25 23:00:30 +0100

    auth guard done

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -37,7 +37,7 @@ interface SigninCredentials {
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com/auth/';
-  signedIn$ = new BehaviorSubject(false);
+  signedIn$ = new BehaviorSubject(null);
 
   constructor(private httpClient: HttpClient) {}
 
