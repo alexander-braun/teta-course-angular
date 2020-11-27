e1e174ee4f3eae31a95955f91ea902a74bc2aa0d
Alex <alexanderbraun@freenet.de>
2020-11-27 22:59:29 +0100

    email-inbox and service wired up

--- emailclient/src/app/inbox/email.service.ts
+++ emailclient/src/app/inbox/email.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
+  rootUrl = 'https://api.angular-email.com';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getEmails(): Observable<EmailSummary[]> {
+    return this.httpClient.get<EmailSummary[]>(this.rootUrl + '/emails');
+  }
 }
