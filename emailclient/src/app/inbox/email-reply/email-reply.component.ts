1296b5a610437d064ca6f27422425d4c008c57b5
Alex <alexanderbraun@freenet.de>
2020-12-12 23:46:56 +0100

    added email reply button

--- emailclient/src/app/inbox/email-reply/email-reply.component.ts
+++ emailclient/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Email } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
 export class EmailReplyComponent implements OnInit {
+  showModal = false;
+  @Input() email: Email;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: 'RE: ' + this.email.subject,
+      text: `\n\n\n ---------- ${
+        this.email.from
+      } wrote:\n> ${this.email.text.trim().replace(/\n/gi, '\n> ')}`,
+    };
   }
 
+  onSubmit(email: Email): void {}
 }
